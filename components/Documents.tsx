81cfcff54f949e53b09792c7c7ed0eeaaa0ab5cc
Ashutosh Savani <ashutosh.savani@brainvire.com@BVIAHMLAP-588.brainvire.com>
2024-08-04 16:21:12 +0530

    Day 2 completed of chat with PDF challange

---
+++ components/Documents.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PlaceHolderDocuments from './PlaceHolderDocuments'
+
+const Documents = () => {
+  return (
+    <div className='flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto'>
+
+        {/* {Mac thtough doc} */}
+        
+      <PlaceHolderDocuments/>
+    </div>
+  )
+}
+
+export default Documents
